397c2eb54c7e5b711541f7db914e8cd2e0820953
Alex Guerrero <alexguerrero1092@gmail.com>
2015-03-15 14:29:44 +0100

    Set PostItem.Name and improve PostItem.Filename

--- models/post.go
+++ models/post.go
@@ -3,14 +3,16 @@ package models
 import (
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/go-zoo/bone"
 	"github.com/russross/blackfriday"
 )
 
 type PostItem struct {
-	Name    string
-	Content []byte
+	Name     string
+	Filename string
+	Content  []byte
 }
 
 type PostList struct {
@@ -18,7 +20,6 @@ type PostList struct {
 }
 
 func GetIndex() PostList {
-	// rw.Write([]byte("Hello"))
 
 	dirFiles, _ := ioutil.ReadDir("./content")
 
@@ -30,10 +31,13 @@ func GetIndex() PostList {
 	for _, file := range dirFiles {
 		if file.Name() != ".DS_Store" {
 
+			filename := strings.Replace(file.Name(), ".md", "", -1)
+			name := setPostName(filename)
+
 			fileContent, _ := ioutil.ReadFile(file.Name())
 			renderedOutput := blackfriday.MarkdownCommon(fileContent)
 
-			postList.Posts[index] = PostItem{Name: file.Name(), Content: renderedOutput}
+			postList.Posts[index] = PostItem{Name: name, Filename: filename, Content: renderedOutput}
 
 			index += 1
 		}
@@ -50,4 +54,9 @@ func GetPost(rw http.ResponseWriter, req *http.Request) {
 
 func GetError(rw http.ResponseWriter, req *http.Request) {
 	rw.Write([]byte("Error"))
+}
+
+func setPostName(filename string) string {
+	nameWithoutExt := strings.Replace(filename, ".md", "", -1)
+	return strings.Replace(nameWithoutExt, "-", " ", -1)
 }
