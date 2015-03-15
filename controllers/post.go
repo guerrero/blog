4402a49a4c866daa4d9fc6771528657f0a2c5be2
Alex Guerrero <alexguerrero1092@gmail.com>
2015-03-15 14:20:35 +0100

    Implement models.GetIndex functionality

--- controllers/post.go
+++ controllers/post.go
@@ -10,7 +10,15 @@ type PostController struct {
 }
 
 func (controller *PostController) ServeIndex(rw http.ResponseWriter, req *http.Request) {
-	models.GetIndex(rw, req)
+	postList := models.GetIndex()
+
+	var postNames string
+
+	for _, post := range postList.Posts {
+		postNames += post.Name
+	}
+
+	rw.Write([]byte(postNames))
 }
 
 func (controller *PostController) ServePost(rw http.ResponseWriter, req *http.Request) {
--- models/post.go
+++ models/post.go
@@ -1,13 +1,45 @@
 package models
 
 import (
+	"io/ioutil"
 	"net/http"
 
 	"github.com/go-zoo/bone"
+	"github.com/russross/blackfriday"
 )
 
-func GetIndex(rw http.ResponseWriter, req *http.Request) {
-	rw.Write([]byte("Hello"))
+type PostItem struct {
+	Name    string
+	Content []byte
+}
+
+type PostList struct {
+	Posts []PostItem
+}
+
+func GetIndex() PostList {
+	// rw.Write([]byte("Hello"))
+
+	dirFiles, _ := ioutil.ReadDir("./content")
+
+	posts := make([]PostItem, len(dirFiles))
+	postList := PostList{Posts: posts}
+
+	index := 0
+
+	for _, file := range dirFiles {
+		if file.Name() != ".DS_Store" {
+
+			fileContent, _ := ioutil.ReadFile(file.Name())
+			renderedOutput := blackfriday.MarkdownCommon(fileContent)
+
+			postList.Posts[index] = PostItem{Name: file.Name(), Content: renderedOutput}
+
+			index += 1
+		}
+	}
+
+	return postList
 }
 
 func GetPost(rw http.ResponseWriter, req *http.Request) {
