e26279ee117de369dddde99cca0564a055c32443
Alex Guerrero <alexguerrero1092@gmail.com>
2015-03-26 11:22:32 +0100

    Implement feature to serve static files

--- app.go
+++ app.go
@@ -16,9 +16,15 @@ func main() {
 
 	router := bone.New()
 
+	router.Get("/public/", http.HandlerFunc(serveStaticFiles))
+
 	router.Get("/posts/:id", http.HandlerFunc(postController.ServePost))
 	router.Get("/", http.HandlerFunc(postController.ServeIndex))
 	router.Get("/:any", http.HandlerFunc(postController.ServeError))
 
 	http.ListenAndServe(SERVER_PORT, router)
+}
+
+func serveStaticFiles(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
 }
